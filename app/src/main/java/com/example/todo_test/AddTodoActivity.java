69b4858ebb58503112b33b6c59092a033d58ba6c
shlomiafr <shlomi.afr@tlvedu.org.il>
2020-03-30 16:48:14 +0300

    Buttons+implementation

--- app/src/main/java/com/example/todo_test/AddTodoActivity.java
+++ app/src/main/java/com/example/todo_test/AddTodoActivity.java
@@ -5,15 +5,52 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddTodoActivity extends AppCompatActivity {
 
-    private static final String TAG = "AddTodoActivity";
-
+    Button addTaskBtn, goBackToListBtn;
+    EditText titleEt, descriptionEt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_todo);
+
+        addTaskBtn =findViewById(R.id.add_new_task_btn);
+        titleEt = findViewById(R.id.new_task_title_ET);
+        descriptionEt = findViewById(R.id.new_task_description_ET);
+
+        addTaskBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String title = titleEt.getText().toString();
+                String description = descriptionEt.getText().toString();
+
+                if (title.equals("") || description.equals("") ){
+                    Toast.makeText(AddTodoActivity.this, "Fill in all the Blanks", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                titleEt.setText("");
+                descriptionEt.setText("");
+
+                Task task = new Task(title,description,Status.TODO);
+                DataManager.getInstance().addTask(task);
+
+            }
+        });
+
+        goBackToListBtn = findViewById(R.id.go_back_to_list);
+        goBackToListBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/todo_test/MainActivity.java
+++ app/src/main/java/com/example/todo_test/MainActivity.java
@@ -3,13 +3,16 @@ package com.example.todo_test;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-
-
+    Button goToAddTodo ;
+    TodoAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +20,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         RecyclerView recyclerView =findViewById(R.id.container);
-        TodoAdapter adapter = new TodoAdapter();
+        adapter = new TodoAdapter();
         recyclerView.setAdapter(adapter);
 
+        goToAddTodo = findViewById(R.id.goto_add_new_task_btn1);
 
+        goToAddTodo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddTodoActivity.class);
+                startActivity(intent);
+            }
+        });
 
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        adapter.notifyDataSetChanged();
     }
 }
